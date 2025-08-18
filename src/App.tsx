37af97ec09cf6ed92ce65e41d6a7fbd18253d15d
Tran Nhan <trannhan@MacBook-Prolocal-2.local>
2025-08-18 15:00:34 +0700

    build fail check

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './components/Login/Login'
-import Home from './components/Home/Home'
+// import Home from './components/Home/Home'
 import './App.css'
 
 // Define app title
